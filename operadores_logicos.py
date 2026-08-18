8703421ea6a87f104ab99f69733331bc9fa47b1e
loangelins <lourenzoanjos75@gmail.com>
2026-08-17 21:38:08 -0300

    Add logical operators examples in operadores_logicos.py

---
+++ operadores_logicos.py
@@ -0,0 +1,40 @@
+# Atividade 1
+idade = 20
+tem_carteira = True
+
+print(idade >= 18 and tem_carteira)
+
+
+# Atividade 2
+
+idade = 16
+tem_autorizacao = True 
+
+print(idade>= 18 or tem_autorizacao)
+
+# Atividade 3
+
+idade = 20
+tem_carteira = False
+
+print(idade >= 18 and tem_carteira)
+
+
+idade = 16
+tem_autorizacao = False
+
+print(idade>= 18 or tem_autorizacao)
+
+# Atividade 4
+
+numero = 10
+numero2 = 5
+
+print( numero2 and numero ==10)
+
+# Atividade 5 
+
+nota = 7
+frequencia = 80
+
+print(6 and frequencia >=75)
